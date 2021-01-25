88c928bdcd26cc1bc935f1cf27ec0ea182ba12e1
Bjørnar <bjornar24@gmail.com>
2021-01-25 22:08:18 +0100

    Fixed compiling for 4.26. Game viewport output is not working (black texture). TextureRenderTarget crashes the editor.

--- Source/SpoutPlugin/Private/SpoutPluginPrivatePCH.h
+++ Source/SpoutPlugin/Private/SpoutPluginPrivatePCH.h
@@ -2,6 +2,11 @@
 
 #include "SpoutModule.h"
 
+// Added to try compiling in 4.26
+#include "Engine/Texture2D.h"
+#include "Rendering/Texture2DResource.h"
+// End fix
+
 // You should place include statements to your module's private header files here.  You only need to
 // add includes for headers that are used in most of your module's source files though.
 
